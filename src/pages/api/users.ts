f5590e826f7f73413386eb08f87b592f916aaedb
Ilay_Arugi <ilay.arugi@gmail.com>
2024-11-23 17:33:21 +0200

    skeleton ready

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Client } from  'pg';
+import pool from '@/lib/db';
 
 // הגדרת טיפוס עבור רשומת משתמש
 interface User {
@@ -11,21 +11,13 @@ interface User {
   permission: 'admin' | 'regular';
 }
 
-// יצירת אובייקט הלקוח של PostgreSQL
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-});
 
 // פונקציית ה-API של Next.js עם טיפוסי פרמטרים
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    await client.connect(); // חיבור למסד הנתונים
     
-    const result = await client.query<User>('SELECT * FROM users'); // שאילתה עם טיפוס מוגדר
+    const result = await pool.query<User>('SELECT * FROM users'); // שאילתה עם טיפוס מוגדר
     res.status(200).json(result.rows);
   } catch (error) {
     res.status(500).json({ error: 'Database connection failed', details: (error as Error).message });
-  } finally {
-    await client.end(); // סגירת החיבור
-  }
-}
+  }}
