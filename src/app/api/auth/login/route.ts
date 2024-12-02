bbb930112940c0c569255fb228086cb5d4c29781
Liel Maoz <lielmaoz1@gmail.com>
2024-12-02 20:06:58 +0200

    Added sign-up front page with form validation

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from 'next/server';
 import { compare } from 'bcrypt';
-import { SignJWT } from 'jose'; 
+import { SignJWT } from 'jose';
 import pool from '@/app/lib/db';
-
-
 
 // This endpoint authenticates the user and generates a JWT token
 export async function POST(req: Request) {
-  const { email, password }: { email: string; password: string } = await req.json();
+  const { email, password }: { email: string; password: string } =
+    await req.json();
 
   // Fetch user from the database
-  const res = await pool.query('select * from users where email=$1',[email]);
+  const res = await pool.query('select * from users where email=$1', [email]);
   const user = res.rows[0];
 
   if (!user) {
@@ -24,19 +23,18 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 });
   }
 
-  // Create JWT token with user id and role 
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET || 'default_secret'); // Make sure to set JWT_SECRET in .env file
-  const jwt = await new SignJWT(
-    {
-      sub: user.email, // The unique user email
-      role: user.permission, // User's role (admin, regular)
-    })
+  // Create JWT token with user id and role
+  const secret = new TextEncoder().encode(
+    process.env.JWT_SECRET || 'default_secret'
+  ); // Make sure to set JWT_SECRET in .env file
+  const jwt = await new SignJWT({
+    sub: user.email, // The unique user email
+    role: user.permission, // User's role (admin, regular)
+  })
     .setProtectedHeader({ alg: 'HS256' }) // Set the algorithm
-    .setIssuedAt() // Automatically sets the timestamp of the JWT creation 
+    .setIssuedAt() // Automatically sets the timestamp of the JWT creation
     .setExpirationTime('2h') // Set expiration time
     .sign(secret); // Sign the token with a secret key
-
-    
 
   // Return JWT token in the response
   return NextResponse.json({ token: jwt });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import HomeCard from './components/HomeCard';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Home</h1>
+        <HomeCard
+          name={'Click to sign-up'}
+          link={'/signup'}
+          imageSrc={'/img2.jpg'}
+          imageAlt={''}
+        />
       </main>
     </div>
   );
